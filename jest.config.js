fb631a749505afb7508a9c941616be6105861c12
Saul Hardman <hello@iamsaul.co.uk>
2020-08-11 12:24:32 +0200

    chore: remove all `::` prefixed output from tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'test';
 const processStdoutWrite = process.stdout.write.bind(process.stdout);
 
 process.stdout.write = (str, encoding, cb) => {
-  if (!str.match(/^::(debug|error)::/)) {
+  if (!str.match(/^::/)) {
     return processStdoutWrite(str, encoding, cb);
   }
 
